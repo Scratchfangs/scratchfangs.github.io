e642af8e1c9eac6f66a8682ea7c7685f0b94ed70
Marcus <techgenius64@gmail.com>
2023-03-26 16:24:11 -0400

    Update deleteextension.js

--- deleteextension.js
+++ deleteextension.js
@@ -1,34 +1,35 @@
 (function (Scratch) {
-'use strict';
-console.log("DeleteSprite v1.0");
+  'use strict';
+  console.log("DeleteSprite v1.0");
 
-class DeleteSprite {
-  getInfo() {
-    return {
-      id: 'DeleteSprite',
-      name: 'Delete Sprite',
-      color1: '#ff6d00',
-      blocks: [
-        {
-          opcode: 'deleteSprite',
-          blockType: Scratch.BlockType.COMMAND,
-          text: 'Delete sprite [SPRITE]',
-          arguments: {
-            SPRITE: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'Sprite1',
+  class DeleteSprite {
+    getInfo() {
+      return {
+        id: 'DeleteSprite',
+        name: 'Delete Sprite',
+        color1: '#ff6d00',
+        blocks: [
+          {
+            opcode: 'deleteSprite',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Delete sprite [SPRITE]',
+            arguments: {
+              SPRITE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Sprite1',
+              },
             },
           },
-        },
-      ],
-    };
+        ],
+      };
+    }
+
+    deleteSprite({ SPRITE }) {
+      const vm = window.vm || document.querySelector("#app")._reactRootContainer.current.child.stateNode.store.getState().scratchGui.vm;
+      vm.deleteSprite(vm.runtime.getSpriteTargetByName(SPRITE).id);
+    }
   }
 
-  deleteSprite({ SPRITE }) {
-    vm.deleteSprite(vm.runtime.getSpriteTargetByName(SPRITE).id)
-  }
-}
-
-Scratch.extensions.register(new DeleteSprite());
-// @ts-ignore
+  Scratch.extensions.register(new DeleteSprite());
+  // @ts-ignore
 })(Scratch);
