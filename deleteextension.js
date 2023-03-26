883a34767dccb454f3c8a325186785deea982ef3
Marcus <techgenius64@gmail.com>
2023-03-26 16:01:33 -0400

    Update deleteextension.js

--- deleteextension.js
+++ deleteextension.js
@@ -1,35 +1,58 @@
 (function (Scratch) {
-'use strict';
-console.log("DeleteSprite v1.0");
-const vm = Scratch.vm;
+  'use strict';
+  console.log("DeleteSprite v1.0");
 
-class DeleteSprite {
-  getInfo() {
-    return {
-      id: 'DeleteSprite',
-      name: 'Delete Sprite',
-      color1: '#ff6d00',
-      blocks: [
-        {
-          opcode: 'deleteSprite',
-          blockType: Scratch.BlockType.COMMAND,
-          text: 'Delete sprite [SPRITE]',
-          arguments: {
-            SPRITE: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'Sprite1',
+  // Wait for the VM to be initialized
+  Scratch.extensions.register(new (class {
+    getInfo() {
+      return {
+        id: 'DeleteSprite',
+        name: 'Delete Sprite',
+        color1: '#ff6d00',
+        blocks: [
+          {
+            opcode: 'deleteSprite',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Delete sprite [SPRITE]',
+            arguments: {
+              SPRITE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Sprite1',
+              },
             },
           },
-        },
-      ],
-    };
+        ],
+      };
+    }
+  })());
+
+  // Define the extension class
+  class DeleteSprite {
+    getInfo() {
+      return {
+        id: 'DeleteSprite',
+        name: 'Delete Sprite',
+        color1: '#ff6d00',
+        blocks: [
+          {
+            opcode: 'deleteSprite',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Delete sprite [SPRITE]',
+            arguments: {
+              SPRITE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Sprite1',
+              },
+            },
+          },
+        ],
+      };
+    }
+
+    deleteSprite({ SPRITE }) {
+      Scratch.vm.deleteSprite(SPRITE);
+    }
   }
 
-  deleteSprite({ SPRITE }) {
-    vm.deleteSprite(SPRITE);
-  }
-}
-
-Scratch.extensions.register(new DeleteSprite());
-// @ts-ignore
+  Scratch.extensions.register(new DeleteSprite());
 })(Scratch);
