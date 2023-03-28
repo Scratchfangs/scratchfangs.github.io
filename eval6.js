50514714546a8cf7b88a7f2d8bdc6708d199f7e7
Marcus <techgenius64@gmail.com>
2023-03-28 19:06:22 -0400

    Create eval6.js

---
+++ eval6.js
@@ -0,0 +1,40 @@
+(function (Scratch) {
+'use strict';
+console.log("Eval for Valuminous");
+
+class evaluate {
+  getInfo() {
+    return {
+      id: 'evaluate',
+      name: 'evaluate',
+      color1: '#ff6d00',
+      blocks: [
+        {
+        opcode: 'evaluate',
+        text: 'eval [JAVASCRIPT]',
+        blockType: Scratch.BlockType.COMMAND,
+                    arguments: {
+                        JAVASCRIPT: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: "console.log('Hello!')"
+            },
+          },
+        },
+      ],
+    };
+  }
+
+  evaluate({ JAVASCRIPT }) {
+        try {
+            // eslint-disable-next-line no-eval
+            eval(String(JAVASCRIPT));
+        } catch (e) {
+            alert(e);
+            console.error(e);
+        }
+  }
+}
+
+Scratch.extensions.register(new evaluate());
+// @ts-ignore
+})(Scratch);
